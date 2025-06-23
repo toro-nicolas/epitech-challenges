cf0f3008d5d21d9e3aed55f3283fb81210c4da9c
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-24 01:40:35 +0200

    ADD: add the index of the server to link all routes

---
+++ server/src/index.ts
@@ -0,0 +1,48 @@
+/* Import modules */
+import dotenv from 'dotenv';
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+
+/* Import database */
+import * as database from './config/database';
+
+/* Import routes */
+import register from './routes/register';
+import login from './routes/login';
+
+
+
+/* Set up app */
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}));
+app.use(express.json());
+
+/* Default route */
+app.get('/', (request: Request, response: Response) => {
+  response.send('Server is running');
+});
+
+/* Set up routes */
+app.use('/api', register);
+app.use('/api', login);
+
+/* Set not found handler */
+app.use((request: Request, response: Response) => {
+  response.status(404).json({ error: "Not found" });
+});
+
+
+
+/* Start the server */
+database.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Server listening on http://localhost:${PORT}/api`);
+  });
+}).catch((err) => {
+  console.error("❌ Failed to connect to database:", err);
+});
