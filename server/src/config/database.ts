ec6b43b3552bc854c0314326a47314d354bef5db
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-24 19:07:47 +0200

    FIX: change the uri of the database used by the server

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -9,7 +9,7 @@ let db_uri: string = process.env.MONGODB_URI as string;
 
 export async function connect(): Promise<void> {
   try {
-    await mongoose.connect(db_uri + "/account");
+    await mongoose.connect(db_uri);
     console.log("Connected to MongoDB successfully!");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
