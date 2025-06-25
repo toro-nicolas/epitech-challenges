b21a549231605dccb198861cb92902cb373c0918
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-25 19:59:09 +0200

    FEAT: link routes with the server api

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,35 +7,61 @@ import cors from 'cors';
 import * as database from './config/database';
 
 /* Import routes */
+
+  /* Account routes */
 import register from './routes/account/register';
 import login from './routes/account/login';
-import accounts from './routes/account/accounts';
+import deleteAccounts from './routes/account/delete-accounts';
+import getAccounts from './routes/account/get-accounts';
+import postAccounts from './routes/account/post-accounts';
+import putAccounts from './routes/account/put-accounts';
+import getMe from './routes/account/get-me';
+import putMe from './routes/account/put-me';
+
+  /* Activity routes */
+import deleteActivities from "./routes/activity/delete-activities";
+import getActivities from './routes/activity/get-activities';
+import postActivities from "./routes/activity/post-activities";
+import putActivities from "./routes/activity/put-activities";
 
 
 
 /* Set up app */
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
 
 /* Default route */
 app.get('/', (request: Request, response: Response) => {
-  response.send('Server is running');
+  response.send('Server is running.');
 });
 
 /* Set up routes */
+
+  /* Account routes */
 app.use('/api', register);
 app.use('/api', login);
-app.use('/api', accounts);
+app.use('/api', deleteAccounts);
+app.use('/api', getAccounts);
+app.use('/api', postAccounts);
+app.use('/api', putAccounts);
+app.use('/api', getMe);
+app.use('/api', putMe);
+
+  /* Activity routes */
+app.use('/api', deleteActivities);
+app.use('/api', getActivities);
+app.use('/api', postActivities);
+app.use('/api', putActivities);
 
 /* Set not found handler */
 app.use((request: Request, response: Response) => {
-  response.status(404).json({ error: "Not found" });
+  response.status(404).json({ error: "Not found." });
 });
 
 
