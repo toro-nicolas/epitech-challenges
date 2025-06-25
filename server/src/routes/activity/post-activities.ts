28970644ee9371fb76c17c672b5409797ebcf125
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-25 19:57:39 +0200

    REFACTOR: refactor the concept and the code of the post-activities

--- server/src/routes/activity/post-activities.ts
+++ server/src/routes/activity/post-activities.ts
@@ -6,35 +6,18 @@ import {authenticateToken} from "../../middleware/auth";
 
 const router = express.Router();
 
-router.post('/activities/:id', authenticateToken, async (request: Request, response: Response): Promise<void> => {
-    const { title, visible, description, challenge, teachers, students, room, start_date, end_data, duration } = request.body;
-    if (!title || !visible || !description || !challenge || !teachers || !students || !room || !start_date || !end_data || !duration) {
-        response.status(400).json({ error: 'All fields are required.' });
-        return;
-    }
-
+router.post('/activities', authenticateToken, async (request: Request, response: Response): Promise<void> => {
     try {
-        const updatedActivity = await Activity.findByIdAndUpdate(
-            request.params.id,
-            {
-                title,
-                visible,
-                description,
-                challenge,
-                teachers,
-                students,
-                room,
-                start_date,
-                end_data,
-                duration
-            },
-            { new: true, runValidators: true }
-        )
-
-        if (!updatedActivity) {
-            response.status(404).json({ error: 'Activity not found.' });
+        if (!request.body.title || !request.body.visible || !request.body.challenge) {
+            response.status(400).json({ error: 'Fields are missing. Please provide title, visibility, and challenge.' });
             return;
         }
+
+        const newActivity = new Activity({
+            ...request.body,
+        });
+        await newActivity.save();
+        response.status(201).json(newActivity);
     } catch (err) {
         response.status(500).json({ message: 'Server error.', details: err });
         console.error("Error creating activity:", err);
