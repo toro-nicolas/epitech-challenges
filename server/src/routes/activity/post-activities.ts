1b3ecb289efcde806332891564c055cef59b61a2
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-25 16:40:53 +0200

    FIX: fix post-activities errors

--- server/src/routes/activity/post-activities.ts
+++ server/src/routes/activity/post-activities.ts
@@ -6,15 +6,38 @@ import {authenticateToken} from "../../middleware/auth";
 
 const router = express.Router();
 
-router.post('/activities', authenticateToken, async (request: Request, response: Response): Promise<void> => {
+router.post('/activities/:id', authenticateToken, async (request: Request, response: Response): Promise<void> => {
+    const { title, visible, description, challenge, teachers, students, room, start_date, end_data, duration } = request.body;
+    if (!title || !visible || !description || !challenge || !teachers || !students || !room || !start_date || !end_data || !duration) {
+        response.status(400).json({ error: 'All fields are required.' });
+        return;
+    }
+
     try {
-        const newActivity = new Activity(request.body);
-        await newActivity.save();
-        response.status(201).json(newActivity);
+        const updatedActivity = await Activity.findByIdAndUpdate(
+            request.params.id,
+            {
+                title,
+                visible,
+                description,
+                challenge,
+                teachers,
+                students,
+                room,
+                start_date,
+                end_data,
+                duration
+            },
+            { new: true, runValidators: true }
+        )
+
+        if (!updatedActivity) {
+            response.status(404).json({ error: 'Activity not found.' });
+            return;
+        }
     } catch (err) {
         response.status(500).json({ message: 'Server error.', details: err });
-        console.log(err);
+        console.error("Error creating activity:", err);
     }
 });
-
 export default router;
