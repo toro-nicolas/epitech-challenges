6e0349a03cd41b6a89d401055322875b1d79b9de
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-27 01:16:22 +0200

    FEAT: add more error management in delete-accounts

--- server/src/routes/account/delete-accounts.ts
+++ server/src/routes/account/delete-accounts.ts
@@ -8,6 +8,12 @@ const router = Router();
 
 router.delete('/accounts/:id', authenticateToken, authorizeAdmin, async (request: Request, response: Response): Promise<void> => {
     try {
+        if (!request.params.id) {
+            response.status(400).json({ error: 'Account ID is required.' });
+            return;
+        }
+        //console.log("Attempting to delete account with ID:", request.params.id);
+
         const deletedAccount = await Account.findByIdAndDelete(request.params.id);
         if (!deletedAccount) {
             response.status(404).json({ error: 'Account not found' });
